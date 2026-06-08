a332f3047767d76bd30950195d1163fb7e020580
Echenko <ehenko.v.g@yandex.ru>
2026-06-08 08:14:29 +0300

    +

--- src/store/selectors/offers-slice.ts
+++ src/store/selectors/offers-slice.ts
@@ -1,7 +1,7 @@
 import { NameSpace } from '../../const';
 import { State } from '../../types/state';
-import { OfferType } from '../../types/offer';
+// import { OfferType } from '../../types/offer';
 
 export const getCheckedEmptyOffers = (state: State): boolean => !state[NameSpace.Offers].offers.length;
-export const getOffers = (state: State): OfferType[] => state[NameSpace.Offers].offers;
+// export const getOffers = (state: State): OfferType[] => state[NameSpace.Offers].offers;
 export const getOffersLoadingStatus = (state: State): boolean | null => state[NameSpace.Offers].offersLoadingStatus;
