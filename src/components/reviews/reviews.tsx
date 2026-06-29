421fccd914198cfabe8490de4822b31749061083
Echenko <ehenko.v.g@yandex.ru>
2026-06-29 22:17:58 +0300

    +

--- src/components/reviews/reviews.tsx
+++ src/components/reviews/reviews.tsx
@@ -4,11 +4,10 @@ import { useAppSelector, useAppDispatch } from '../../hooks/hooks';
 import { useEffect } from 'react';
 import { fetchCommentsOfferAction } from '../../store/api-actions';
 import { useParams } from 'react-router-dom';
-import { AuthorizationStatus, TYPE_OF_ERROR, REVIEW_OFFER } from '../../const';
+import { AuthorizationStatus, TYPE_OF_ERROR } from '../../const';
 import { getSelectedOfferCommentsLoadingStatus } from '../../store/selectors/offer-slice';
 import { Message } from '../message/message';
 import { setErrorType } from '../../store/action';
-import { sortCommentsByDate } from '../../utils';
 
 function Reviews(): JSX.Element {
   const dispatch = useAppDispatch();
@@ -34,7 +33,7 @@ function Reviews(): JSX.Element {
       </h2>
       {!selectedOfferCommentsLoadingStatus &&
         <Message />}
-      <ReviewsList comments={sortCommentsByDate(comments).slice(comments.length - REVIEW_OFFER.MAX_COMMENTS_COUNT)} />
+      <ReviewsList comments={comments} />
       {statusAuthorization === AuthorizationStatus.Auth && <ReviewsForm />}
     </section>
   );
